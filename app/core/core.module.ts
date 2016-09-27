0003f78d7b199f40149841b2bc7722f6cc1d6d8e
Ueeck <William.Ueeck@energytransfer.com>
2016-09-27 13:45:52 -0500

    Missed a file for updating in memory module to 0.1.0

--- app/core/core.module.ts
+++ app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }     from '@angular/core';
 import { HttpModule }   from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { InMemoryWebApiModule }	from 'angular2-in-memory-web-api';
+import { InMemoryWebApiModule }	from 'angular-in-memory-web-api';
 
 import { SharedModule }			from '../shared/shared.module';
 import { NoteModule }   from '../notes/note.module';
